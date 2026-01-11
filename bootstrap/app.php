514bac275dd83497ea3f54011fe6f080c2d29b0f
ikue <hmn227@icloud.com>
2026-01-11 23:05:50 +0900

    プロキシを信頼する設定を追加

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\AdminMiddleware::class,
+        ]);
+
         $middleware->redirectTo(
             guests: '/login',
             users: '/',
